f67c653e8bfbc37868e258a93ffe1853c31c2750
Jhenerson Silva do Rosario <129077562+JhenersonRosario@users.noreply.github.com>
2023-12-11 18:39:05 -0800

    Delete Semana_02_Aula_01 directory

--- Semana_02_Aula_01/Exercicio1b.py
+++
@@ -1,3 +0,0 @@
-farenheit = float(input("Insira a temperatura em Farenheit:"))
-celsius = (farenheit-32)* 5/9
-print(farenheit,"Farenheit convertido para Celsius é:" ,celsius) 
--- Semana_02_Aula_01/Exercício1a.py
+++
@@ -1,3 +0,0 @@
-celcius = float(input("Insira a quantidade de graus celsius :"))
-farenheit = (celcius*1.8) + 32
-print(celcius, "convertido para farenheit é:" ,farenheit)
